0cd4e1bd8361d28d0b959efda5ccd9ca45d27759
Zack Stout <zackstout@gmail.com>
2018-03-25 20:07:28 -0500

    polar transform finally working

--- client.js
+++ client.js
@@ -2,47 +2,18 @@
 function setup() {
   createCanvas(600, 600);
   background(240);
-  var numCircles = 900;
+  var numCircles = 90;
+  var numDiscs = 30;
+  var maxWidth = 500;
 
   push();
   translate(width/2, height/2);
-  // for (var i=0; i < numCircles; i++) {
-  //   var angle = 2*PI*i/numCircles;
-  //
-  //   // all right let's take a stab at continuous color transition:
-  //   // maybe should just use HSL because we'll want to change brightness..... could use a i guess (transparency).
-  //
-  //   // console.log(i * PI / numCircles);
-  //   // cos wave from 0 to 2*PI:
-  //   var r = 255/2 + 255/2 * cos(i * 2 * PI / numCircles);
-  //   // sin wave from 0 to PI:
-  //   var g = 255 * sin(i * PI / numCircles);
-  //   console.log(r);
-  //
-  //   var b;
-  //   if (i < numCircles/2) {
-  //     b = 0;
-  //   } else if (i > 9 * numCircles/10) {
-  //     b = 100 * (numCircles - i) / numCircles;
-  //   } else if (i > 3 * numCircles/5) {
-  //     b = 255;
-  //   } else {
-  //     b = 100;
-  //   }
-  //   var c = color(r, g, b);
-  //   fill(c);
-  //   noStroke();
-  //   ellipse(200 * cos(angle), 200 * sin(angle), 3, 3);
-  // }
 
-
-  var numDiscs = 100;
-  var maxWidth = 300;
   //Oh, HSL is made with this idea in mind LOL:
   for (var i=0; i < numCircles; i++) {
     // rotate(0.1);
     for (var j=0; j < numDiscs; j++) {
-      var angle = 2 * PI * i / numCircles; // adding Math.random() here: nice
+      var angle = 2 * PI * i / numCircles + Math.random(); // adding Math.random() here: nice
       var radius = j * maxWidth/numDiscs;
       // console.log(radius);
       var brightness = parseInt(radius * 100 / maxWidth);
@@ -52,13 +23,50 @@ function setup() {
       noStroke();
       var c = color('hsb(' + hue + ', 100%, ' + brightness + '%)');
       fill(c);
-      ellipse(radius*cos(angle), radius*sin(angle), 5, 5);
+      var randomRadius = 25 * Math.random();
+      ellipse(radius*cos(angle), radius*sin(angle), randomRadius, randomRadius);
     }
 
   }
 
 
   pop();
+}
+
+function mousePressed() {
+  // console.log(mouseX, mouseY);
+  getPolar(mouseX, mouseY);
+
+}
+
+function getPolar(x, y) {
+  var newX = x - 300;
+  // reverses the y-is-down thing, we'll see if it breaks anything. shouldn't matter, it's just something we're going to translate into proper angle with ugly switch:
+  var newY = 300 - y;
+  // console.log(new);
+  var radius = dist(0, 0, newX, newY);
+
+  var slope = newY / newX;
+  var angle = atan(slope);
+  // console.log('angle' , angle);
+
+  var realAngle;
+
+  if (newX < 0 && newY < 0) {
+    console.log('LL');
+    realAngle = PI + angle;
+  } else if (newX < 0 && newY > 0) {
+    console.log('LG');
+    realAngle = PI + angle;
+  } else if (newX > 0 && newY < 0) {
+    console.log('GL');
+    realAngle = 2*PI + angle;
+  } else if (newX > 0 && newY > 0) {
+      console.log("GG");
+    realAngle = angle;
+  }
+
+  console.log(realAngle);
 }
 
 function draw() {
